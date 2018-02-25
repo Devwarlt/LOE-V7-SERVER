c6adac3397e33109bcc04b764d7fe6d57c6c0669
Devwarlt <devwarlt@loesoft.org>
2018-02-25 00:33:56 -0300

    Fixed few behaviors and restart conflicts

--- common/_appengine_only.bat
+++ common/_appengine_only.bat
@@ -1,18 +1,11 @@
 FOR /F "tokens=4 delims= " %%P IN ('netstat -a -n -o ^| findstr :5555') DO @ECHO TaskKill.exe /PID %%P
-taskkill /f /im appengine.exe
+taskkill /f /im LoESoft.AppEngine.exe
 FOR /F "tokens=4 delims= " %%P IN ('netstat -a -n -o ^| findstr :5555') DO @ECHO TaskKill.exe /PID %%P
-taskkill /f /im appengine.exe
-FOR /F "tokens=4 delims= " %%P IN ('netstat -a -n -o ^| findstr :5555') DO @ECHO TaskKill.exe /PID %%P
-taskkill /f /im appengine.exe
-FOR /F "tokens=4 delims= " %%P IN ('netstat -a -n -o ^| findstr :5555') DO @ECHO TaskKill.exe /PID %%P
-taskkill /f /im appengine.exe
+taskkill /f /im LoESoft.AppEngine.exe
 FOR /F "tokens=4 delims= " %%P IN ('netstat -a -n -o ^| findstr :3000') DO @ECHO TaskKill.exe /PID %%P
-taskkill /f /im appengine.exe
+taskkill /f /im LoESoft.AppEngine.exe
 FOR /F "tokens=4 delims= " %%P IN ('netstat -a -n -o ^| findstr :3000') DO @ECHO TaskKill.exe /PID %%P
-taskkill /f /im appengine.exe
-FOR /F "tokens=4 delims= " %%P IN ('netstat -a -n -o ^| findstr :3000') DO @ECHO TaskKill.exe /PID %%P
-taskkill /f /im appengine.exe
-FOR /F "tokens=4 delims= " %%P IN ('netstat -a -n -o ^| findstr :3000') DO @ECHO TaskKill.exe /PID %%P
-taskkill /f /im appengine.exe
-start appengine.exe
+taskkill /f /im LoESoft.AppEngine.exe
+cd bin-server
+start LoESoft.AppEngine.exe
 exit
