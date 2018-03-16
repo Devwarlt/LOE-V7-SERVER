8d2a8ff2a45810fe149d9a36804bfc0d32f5b4b3
Filisha <30576431+Filisha@users.noreply.github.com>
2018-03-15 21:26:11 -0400

    Organized stuff
    organized some shtuff for shatts

---
+++ gameserver/logic/monsters/Dungeons/The Shatters/Birds/Blizzard.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoESoft.GameServer.logic.monsters.Dungeons.The_Shatters.Birds
+{
+	class Blizzard
+	{
+	}
+}
