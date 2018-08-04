052338879d7b5f102305402d0523513f1e062664
Devwarlt <devwarlt@loesoft.org>
2018-08-04 12:05:14 -0300

    New list type for Item Data.

---
+++ common/assets/itemdata/Vocations.cs
@@ -0,0 +1,11 @@
+﻿namespace LoESoft.Core.assets.itemdata
+{
+    public enum Vocations
+    {
+        ANY = -1,
+        APPRENTICE = 0,
+        MAGICIAN = 1,
+        KNIGHT = 2,
+        HUNTER = 3
+    }
+}
