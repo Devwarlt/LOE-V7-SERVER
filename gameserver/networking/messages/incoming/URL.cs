fbdc6712f355e494eba67c89f8c25a817c056003
Devwarlt <devwarlt@loesoft.org>
2018-10-18 00:39:36 -0300

    Revert "Revert "Get URL by PlayerType.""
    This reverts commit 7e0f78bc16c50369665326adb704d751ba010f09.

---
+++ gameserver/networking/messages/incoming/URL.cs
@@ -0,0 +1,21 @@
+﻿#region
+
+using LoESoft.Core;
+
+#endregion
+
+namespace LoESoft.GameServer.networking.incoming
+{
+    public class URL : IncomingMessage
+    {
+        public string Domain { get; set; }
+
+        public override MessageID ID => MessageID.URL;
+
+        public override Message CreateInstance() => new URL();
+
+        protected override void Read(NReader rdr) => Domain = rdr.ReadUTF();
+
+        protected override void Write(NWriter wtr) => wtr.WriteUTF(Domain);
+    }
+}
