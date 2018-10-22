1aee8f17ff6dd9a803c169408b4cc100ca127ef4
Devwarlt <devwarlt@loesoft.org>
2018-10-22 02:20:06 -0200

    Update gameserver/realm/world/Sight.cs

--- gameserver/realm/world/Sight.cs
+++ gameserver/realm/world/Sight.cs
@@ -1,68 +1,23 @@
 ﻿#region
 
 using LoESoft.GameServer.realm.entity.player;
-using System;
 using System.Collections.Generic;
 
 #endregion
 
 namespace LoESoft.GameServer.realm
 {
-    internal static class Sight
+    public static class Sight
     {
-        private static readonly Dictionary<int, IntPoint[]> points = new Dictionary<int, IntPoint[]>();
-
-        public static List<IntPoint> GetSquare(int radius)
+        public static List<IntPoint> GetSquare(Player player, int radius)
         {
-            List<IntPoint> square = new List<IntPoint>();
+            var square = new List<IntPoint>();
 
-            for (int x = -radius; x <= radius; x++)
-                for (int y = -radius; y <= radius; y++)
+            for (var x = -radius; x <= radius; x++)
+                for (var y = -radius; y <= radius; y++)
                     square.Add(new IntPoint(x, y));
 
             return square;
-        }
-
-        public static IntPoint[] GetSightCircle(int radius)
-        {
-            if (!points.TryGetValue(radius, out IntPoint[] ret))
-            {
-                List<IntPoint> pts = new List<IntPoint>();
-                for (int y = -radius; y <= radius; y++)
-                    for (int x = -radius; x <= radius; x++)
-                    {
-                        if (x * x + y * y <= radius * radius)
-                            pts.Add(new IntPoint(x, y));
-                    }
-                ret = points[radius] = pts.ToArray();
-            }
-            return ret;
-        }
-
-        public static IntPoint[] RayCast(Player player, int radius = 15)
-        {
-            List<IntPoint> RayTiles = new List<IntPoint>();
-            int angle = 0;
-            while (angle < 360)
-            {
-                int distance = 0;
-                while (distance < radius)
-                {
-                    int x = (int)(distance * Math.Cos(angle));
-                    int y = (int)(distance * Math.Sin(angle));
-                    if ((x * x + y * y) <= (radius * radius))
-                    {
-                        RayTiles.Add(new IntPoint(x, y));
-                        GameServer.Manager.GameData.ObjectDescs.TryGetValue(player.Owner.Map[(int)player.X + x, (int)player.Y + y].ObjType, out ObjectDesc desc);
-                        if (desc != null && desc.BlocksSight)
-                            break;
-                        RayTiles.Add(new IntPoint(x, y));
-                    }
-                    distance++;
-                }
-                angle++;
-            }
-            return RayTiles.ToArray();
         }
     }
 }
